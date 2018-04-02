fedce758b3ca5885527a6c96e179402e7cca133c
romapanchyshyn <roma.panchyshyn@gmail.com>
2018-04-02 12:01:09 +0300

    add text in branch new_class

--- src/com/epam/lab/task6/NewClassForBranch.java
+++ src/com/epam/lab/task6/NewClassForBranch.java
@@ -3,5 +3,6 @@ package com.epam.lab.task6;
 public class NewClassForBranch {
     public static void printInNewBranch(){
         System.out.println("New branch in program");
+        System.out.println("Text in branch new_class");
     }
 }
