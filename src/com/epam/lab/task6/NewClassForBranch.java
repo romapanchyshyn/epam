6a41d5abf9e6b49e8663bacf29830578866d647f
romapanchyshyn <roma.panchyshyn@gmail.com>
2018-04-02 19:10:54 +0300

    Commit

--- src/com/epam/lab/task6/NewClassForBranch.java
+++ src/com/epam/lab/task6/NewClassForBranch.java
@@ -2,7 +2,6 @@ package com.epam.lab.task6;
 
 public class NewClassForBranch {
     public static void printInNewBranch(){
-        System.out.println("New branch in program");
-        System.out.println("Text in branch new_class");
+        System.out.println("1234");
     }
 }
