55bdb5a98014232aa61e5d1b19a18ebe02050c6a
romapanchyshyn <roma.panchyshyn@gmail.com>
2018-04-02 12:33:40 +0300

    h Please enter the commit message for your changes. Lines starting e

--- src/com/epam/lab/task6/NewClassForBranch.java
+++ src/com/epam/lab/task6/NewClassForBranch.java
@@ -3,7 +3,7 @@ package com.epam.lab.task6;
 public class NewClassForBranch {
     public static void printInNewBranch(){
         System.out.println("New branch in program");
-        System.out.println("Text in branch new_class");
+        System.out.println("Change text for merge");
         System.out.println("Another new text in branch new_class");
     }
 }
